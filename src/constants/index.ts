80e7e30d37fc5d391bbf9e0912b63f307ce5ad81
Nikita Stroganov <nstroganov@adsterra.com>
2021-09-17 14:30:06 +0300

    fix map

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,4 @@ export const RUHUNT_DISCORD = 'https://discord.gg/eDEQ5RFp4F';
 export const RUHUNT_FORM_RIVALS_1 =
   'https://docs.google.com/forms/d/1VWKnxc4JgqKg4XtLc4IwMkKLucEdl5ZMZSLqQGfS80g/edit?usp=sharing';
 
-export const IS_MAP_OFF = true;
+export const IS_MAP_OFF = false;
